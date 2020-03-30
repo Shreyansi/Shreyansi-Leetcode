314802ea9fd2b698fe293ac516003aa82d44228e
sidshreya <kumar.shreyansi@gmail.com>
2020-03-30 11:37:50 -0700

    Min Stack implementation

---
+++ Design/src/MinStack.java
@@ -0,0 +1,54 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+
+public class MinStack {
+	
+	
+	Deque<Integer> dq ;
+    PriorityQueue<Integer> pq;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        dq = new ArrayDeque<Integer>();
+        pq = new PriorityQueue<Integer>();
+        
+    }
+    
+    public void push(int x) {
+        dq.push(x);
+        pq.offer(x);
+    }
+    
+    public void pop() {
+        int x = dq.pop();
+        pq.remove(x);
+    }
+    
+    public int top() {
+        return dq.peek();
+    }
+    
+    public int getMin() {
+        return pq.peek();
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MinStack obj = new MinStack();
+		 obj.push(2);
+		 obj.push(1);
+		 obj.push(3);
+		 obj.push(4);
+		 obj.push(8);
+		 //int param_2 = obj.pop();
+		 //System.out.println("Element popped from stack "+param_2);
+		 int param_3 = obj.top();
+		 int param_4 = obj.getMin();
+		
+		System.out.println("Top of stack "+param_3);
+		System.out.println("Minimum of stack "+param_4);
+
+	}
+
+}
